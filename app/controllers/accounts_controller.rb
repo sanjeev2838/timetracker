123276ac1e36414f9da14daca30d18d0d3fdc00d
sanjeev <sanjeev2838@gmail.com>
2014-11-29 11:07:57 +0530

    1.) Added devise user model 2.) Association with Account 3.) rspec tests 4.( Needs to revise it later)

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,9 @@
 class AccountsController < ApplicationController
   def new
     @account = Account.new
+    @account.build_owner
   end
+
   def create
     @account = Account.new(account_params)
     if @account.save
@@ -10,8 +12,9 @@ class AccountsController < ApplicationController
       render action: 'new'
     end
   end
+
   private
   def account_params
-    params.require(:account).permit(:subdomain)
+    params.require(:account).permit(:subdomain, owner_attributes: [:name, :email, :password, :password_confirmation])
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Account, :type => :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   describe 'validations' do
+    it { should validate_presence_of :owner }
     it { should validate_presence_of :subdomain }
     it { should validate_uniqueness_of :subdomain }
     it { should allow_value('bolandrm').for(:subdomain) }
@@ -19,6 +20,9 @@ RSpec.describe Account, :type => :model do
   end
 
   describe 'associations' do
-    it 'should have owner'
+    # it 'should have owner'
+    describe 'associations' do
+      it { should belong_to :owner }
+    end
   end
 end
